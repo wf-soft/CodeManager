d224729b1ed361b0eee6a8cdc598427fba7159ca
wf-so <wf-soft@qq.com>
2025-03-20 13:32:24 +0800

    1.0.9

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import { Main } from "@commands/index";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  vscode.l10n;
   Main.activate(context);
 }
 
